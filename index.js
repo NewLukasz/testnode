5dcc28c62c34eabe6d8e7ca1fb60afff750cd0aa
Łukasz Kilijański <55434595+NewLukasz@users.noreply.github.com>
2024-08-21 15:32:07 +0200

    Update index.js
    Testowe zmiany

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
 
+alert(10);
+
 const server = http.createServer((req, res) => {
+  alert(20);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
+alert(30);
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+
+alert(40);
