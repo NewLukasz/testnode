1ca0e39a3ebf16288786135dacd7d053492c8b8a
Łukasz Kilijański <55434595+NewLukasz@users.noreply.github.com>
2024-08-21 15:39:33 +0200

    Update index.js
    test

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const http = require('http');
 
-console.log('TEST');
-alert(10);
-
 const server = http.createServer((req, res) => {
-  alert(20);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello Worldfhwejrfherifhjiehrfir\n');
 });
 
-alert(30);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
 
-alert(40);
+
